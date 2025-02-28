7779bdc6907c2688fb692128a21d6b04b68e86e0
Irene Rodríguez <irene.rodrod.2@educa.jcyl.es>
2025-02-28 10:10:28 +0100

    Primer commit

---
+++ EJ01.java
@@ -0,0 +1,32 @@
+// 1.  Realiza el pseudocódigo y la codificación en lenguaje de programación
+// Java de un algoritmo que solicite por teclado tres números y muestre por
+// pantalla un mensaje indicando si los tres números son iguales, si los tres
+// son distintos o si hay dos iguales.
+
+import java.util.Scanner;
+
+public class EJ01 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+		int n1, n2, n3;
+		
+		System.out.print("Introduzca el primer número: ");
+		n1=teclado.nextInt();
+		System.out.print("Introduzca el segundo número: ");
+		n2=teclado.nextInt();
+		System.out.print("Introduzca el tercer número: ");
+		n3=teclado.nextInt();
+		
+		if (n1==n2 && n2==n3 && n1==n3) {
+			System.out.print("Los tres números son iguales.");
+		}
+		else { //else if (n1!=n2 && n2!=n3 && n1!=n3) {
+			if (n1!=n2 && n2!=n3 && n1!=n3) {
+			System.out.print("Los tres números son distintos.");
+			}
+			else {
+			System.out.print ("Dos números son diferentes.");
+			}
+		}
+	}
+}
